826ce2555cd05f0fe7faccc5dc12db0270a5ea3f
Mukut <87146309+webmukut@users.noreply.github.com>
2021-11-17 03:29:20 +0600

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,9 @@
 // As A Vanilla JavaScript Plugin
 var gallery = new SimpleLightbox('.gallery a', { 
   /* options */
-  // spinner:true,
-  // overlay:true,
-	// nav:true, 
+  spinner:true,
+  overlay:true,
+	nav:true, 
 });
 
 // As A jQuery Plugin -->
