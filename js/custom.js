8b62d1d576eed7d5460b2101219f84ffac3919d8
Mukut <87146309+webmukut@users.noreply.github.com>
2021-11-17 03:21:31 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+// As A Vanilla JavaScript Plugin
+var gallery = new SimpleLightbox('.gallery a', { 
+  /* options */
+  // spinner:true,
+  // overlay:true,
+	// nav:true, 
+});
+
+// As A jQuery Plugin -->
+var gallery = $('.gallery a').simpleLightbox({
+  /* options */
+});
