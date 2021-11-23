0b83bf877e921b63492962c23ef501f30f0c0479
Mukut <87146309+webmukut@users.noreply.github.com>
2021-11-24 03:53:36 +0600

    Add files via upload

---
+++ js/facebookicon.js
@@ -0,0 +1,176 @@
+function keya() {
+    var x = document.getElementById("Suji");
+    if (x.style.transform === "scale(1)") {
+      x.style.transform = "scale(0)";
+    } else {
+      x.style.transform = "scale(1)";
+    }
+  }
+  function rejowana() {
+    var x = document.getElementById("tania");
+    if (x.style.transform === "scale(1)") {
+      x.style.transform = "scale(0)";
+    } else {
+      x.style.transform = "scale(1)";
+    }
+  }
+  function abdul() {
+    var x = document.getElementById("amit");
+    if (x.style.transform === "scale(1)") {
+      x.style.transform = "scale(0)";
+    } else {
+      x.style.transform = "scale(1)";
+    }
+  }
+  function asif() {
+    var x = document.getElementById("patwary");
+    if (x.style.transform === "scale(1)") {
+      x.style.transform = "scale(0)";
+    } else {
+      x.style.transform = "scale(1)";
+    }
+  }
+  function fahim() {
+    var x = document.getElementById("ahmed1");
+    if (x.style.transform === "scale(1)") {
+      x.style.transform = "scale(0)";
+    } else {
+      x.style.transform = "scale(1)";
+    }
+  }
+  function hai() {
+    var x = document.getElementById("abdul");
+    if (x.style.transform === "scale(1)") {
+      x.style.transform = "scale(0)";
+    } else {
+      x.style.transform = "scale(1)";
+    }
+  }
+  function hasibul() {
+    var x = document.getElementById("shanto");
+    if (x.style.transform === "scale(1)") {
+      x.style.transform = "scale(0)";
+    } else {
+      x.style.transform = "scale(1)";
+    }
+  }
+  function iqbal() {
+    var x = document.getElementById("ahmed2");
+    if (x.style.transform === "scale(1)") {
+      x.style.transform = "scale(0)";
+    } else {
+      x.style.transform = "scale(1)";
+    }
+  }
+  function mahbub() {
+    var x = document.getElementById("selim");
+    if (x.style.transform === "scale(1)") {
+      x.style.transform = "scale(0)";
+    } else {
+      x.style.transform = "scale(1)";
+    }
+  }
+  function mukut() {
+    var x = document.getElementById("roy");
+    if (x.style.transform === "scale(1)") {
+      x.style.transform = "scale(0)";
+    } else {
+      x.style.transform = "scale(1)";
+    }
+  }
+  function ms() {
+    var x = document.getElementById("mihad");
+    if (x.style.transform === "scale(1)") {
+      x.style.transform = "scale(0)";
+    } else {
+      x.style.transform = "scale(1)";
+    }
+  }
+  function muksitur() {
+    var x = document.getElementById("hamim");
+    if (x.style.transform === "scale(1)") {
+      x.style.transform = "scale(0)";
+    } else {
+      x.style.transform = "scale(1)";
+    }
+  }
+  function mursalin() {
+    var x = document.getElementById("ahmed3");
+    if (x.style.transform === "scale(1)") {
+      x.style.transform = "scale(0)";
+    } else {
+      x.style.transform = "scale(1)";
+    }
+  }
+  function opu() {
+    var x = document.getElementById("mohajon");
+    if (x.style.transform === "scale(1)") {
+      x.style.transform = "scale(0)";
+    } else {
+      x.style.transform = "scale(1)";
+    }
+  }
+  function rahat() {
+    var x = document.getElementById("flr");
+    if (x.style.transform === "scale(1)") {
+      x.style.transform = "scale(0)";
+    } else {
+      x.style.transform = "scale(1)";
+    }
+  }
+  function sabbir() {
+    var x = document.getElementById("mridul");
+    if (x.style.transform === "scale(1)") {
+      x.style.transform = "scale(0)";
+    } else {
+      x.style.transform = "scale(1)";
+    }
+  }
+  function sazidul() {
+    var x = document.getElementById("islam");
+    if (x.style.transform === "scale(1)") {
+      x.style.transform = "scale(0)";
+    } else {
+      x.style.transform = "scale(1)";
+    }
+  }
+  function shahed() {
+    var x = document.getElementById("khan");
+    if (x.style.transform === "scale(1)") {
+      x.style.transform = "scale(0)";
+    } else {
+      x.style.transform = "scale(1)";
+    }
+  }
+  function shaikh() {
+    var x = document.getElementById("smash");
+    if (x.style.transform === "scale(1)") {
+      x.style.transform = "scale(0)";
+    } else {
+      x.style.transform = "scale(1)";
+    }
+  }
+  function sajib() {
+    var x = document.getElementById("shaha");
+    if (x.style.transform === "scale(1)") {
+      x.style.transform = "scale(0)";
+    } else {
+      x.style.transform = "scale(1)";
+    }
+  }
+  function shakil() {
+    var x = document.getElementById("skr");
+    if (x.style.transform === "scale(1)") {
+      x.style.transform = "scale(0)";
+    } else {
+      x.style.transform = "scale(1)";
+    }
+  }
+  function sifat() {
+    var x = document.getElementById("hasan");
+    if (x.style.transform === "scale(1)") {
+      x.style.transform = "scale(0)";
+    } else {
+      x.style.transform = "scale(1)";
+    }
+  }
